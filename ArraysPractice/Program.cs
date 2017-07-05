c8ff09e9e54c96843d3a6149e3f83aba86e4d25f
clarecorr <clare.e.corrigan@gmail.com>
2017-07-05 11:09:42 -0400

    several in class practice problems

--- ArraysPractice/Program.cs
+++ ArraysPractice/Program.cs
@@ -10,6 +10,41 @@ namespace ArraysPractice
     {
         static void Main(string[] args)
         {
+            ////declaring an array without initializing
+            //int[] myFirstArray;
+            ////declaring and initializing a second array
+            //string[] myStringArray = { "first", "second", "third", "fourth" };
+            ////declare and initialize a char array with the letters of your last name
+            //char[] myLastName = { 'c', 'o', 'r', 'r', 'i', 'g', 'a', 'n' };
+            ////print three elements of my array
+            //Console.WriteLine(myLastName[0]);
+            //Console.WriteLine(myLastName[6]);
+            //Console.WriteLine(myLastName[7]);
+
+            ////create the following arrays and print using the correct index numbers
+            ////array of top ten vacay spots
+            //string[] topTenVacaySpots = { "Rome", "Cancun", "London", "Miami", "Orlando", "San Francisco", "Myrtle Beach", "Branson", "Paris", "New York City" };
+            ////print the first and last spots
+            //Console.WriteLine(topTenVacaySpots[0]);
+            //Console.WriteLine(topTenVacaySpots[9]);
+            ////make the array of the ages of three children you know
+            ////print each child's age
+            //int[] childrensAges = { 6, 1, 3 };
+            //Console.WriteLine(childrensAges[0]);
+            //Console.WriteLine(childrensAges[1]);
+            //Console.WriteLine(childrensAges[2]);
+            ////make array of gpas on 4.0 scale of 8 students - don't have to print
+            //double[] gpa = { 1, 1.2, 2, 4, 3.5, 3, 2.8, 3.2 };
+            ////make array of first letter of 4 of your classmates names 
+            //char[] initials = { 's', 'j', 'm', 'f' };
+            ////print all four on same line
+            //Console.Write(initials[0]);
+            //Console.Write(initials[1]);
+            //Console.Write(initials[2]);
+            //Console.WriteLine(initials[3]);
+            //Console.WriteLine(initials);
+            //Console.WriteLine("{0}{1}{2}{3}", initials[0], initials[1], initials[2], initials[3]);
+
         }
     }
 }
